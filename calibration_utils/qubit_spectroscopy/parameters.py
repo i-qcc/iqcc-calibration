1c0d8ac6f9acc58ed385179d36c119ba81712ebe
omrieoqm <omrie.ovdat@quantum-machines.co>
2025-08-17 10:47:43 +0300

    default parameters chage in qubit spec and qubit spec vs flux

--- calibration_utils/qubit_spectroscopy/parameters.py
+++ calibration_utils/qubit_spectroscopy/parameters.py
@@ -13,8 +13,8 @@ class NodeSpecificParameters(RunnableParameters):
     """Step size for frequency sweep in MHz. Default is 0.25 MHz."""
     operation: str = "saturation"
     """Type of operation to perform. Default is "saturation"."""
-    operation_amplitude_factor: float = 1.0
-    """Amplitude pre-factor for the operation. Default is 1.0."""
+    operation_amplitude_factor: float = 0.1
+    """Amplitude pre-factor for the operation. Default is 0.1."""
     operation_len_in_ns: Optional[int] = None
     """Length of the operation in nanoseconds. Default is the predefined pulse length."""
     target_peak_width: float = 3e6
