a926e826b53afa626a34b9f49dc393bbdca115b6
Serwan Asaad <serwan.asaad@quantum-machines.co>
2025-08-19 10:57:56 +0200

    Fix: Use QUAM's native save/load

--- iqcc_calibration_tools/quam_config/components/quam_root.py
+++ iqcc_calibration_tools/quam_config/components/quam_root.py
@@ -13,7 +13,9 @@ from qualang_tools.results.data_handler import DataHandler
 
 from dataclasses import field
 from typing import Dict, ClassVar, Sequence, Union
-from iqcc_cloud_client import CloudQuantumMachinesManager # from ..cloud_infrastructure import CloudQuantumMachinesManager
+from iqcc_cloud_client import (
+    CloudQuantumMachinesManager,
+)  # from ..cloud_infrastructure import CloudQuantumMachinesManager
 
 from quam_builder.architecture.superconducting.qpu import FluxTunableQuam
 
@@ -25,39 +27,16 @@ class Quam(FluxTunableQuam):
     """Example Quam root component with enhanced functionality."""
 
     _data_handler: ClassVar[DataHandler | None] = None
-    
+
     twpas: Dict[str, TWPA] = field(default=dict)
-
-    @classmethod
-    def load(cls, *args, **kwargs) -> "Quam":
-        if not args:
-            if "QUAM_STATE_PATH" in os.environ:
-                args = (os.environ["QUAM_STATE_PATH"],)
-            else:
-                raise ValueError(
-                    "No path argument provided to load the QuAM state. "
-                    "Please provide a path or set the 'QUAM_STATE_PATH' environment variable. "
-                    "See the README for instructions."
-                )
-        return super().load(*args, **kwargs)
-
-    def save(
-        self,
-        path: Union[Path, str] | None = None,
-        content_mapping: Dict[Union[Path, str], Sequence[str]] | None = None,
-        include_defaults: bool = False,
-        ignore: Sequence[str] | None = None,
-    ):
-        if path is None and "QUAM_STATE_PATH" in os.environ:
-            path = os.environ["QUAM_STATE_PATH"]
-
-        super().save(path, content_mapping, include_defaults, ignore)
 
     @property
     def data_handler(self) -> DataHandler:
         """Return the existing data handler or open a new one to conveniently handle data saving."""
         if self._data_handler is None:
-            self._data_handler = DataHandler(root_data_folder=self.network["data_folder"])
+            self._data_handler = DataHandler(
+                root_data_folder=self.network["data_folder"]
+            )
             DataHandler.node_data = {"quam": "./state.json"}
         return self._data_handler
 
@@ -67,11 +46,12 @@ class Quam(FluxTunableQuam):
         Returns: the opened Quantum Machine Manager.
         """
         if self.network.get("cloud", False):
-            self.qmm = CloudQuantumMachinesManager(self.network["quantum_computer_backend"])
+            self.qmm = CloudQuantumMachinesManager(
+                self.network["quantum_computer_backend"]
+            )
         else:
             settings = dict(
-                host=self.network["host"],
-                cluster_name=self.network["cluster_name"]
+                host=self.network["host"], cluster_name=self.network["cluster_name"]
             )
 
             if "port" in self.network:
@@ -104,7 +84,9 @@ class Quam(FluxTunableQuam):
             try:
                 self.qubits[name].calibrate_octave(QM)
             except NoCalibrationElements:
-                print(f"No calibration elements found for {name}. Skipping calibration.")
+                print(
+                    f"No calibration elements found for {name}. Skipping calibration."
+                )
 
 
 @quam_dataclass
