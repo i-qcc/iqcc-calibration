634ced181d41e3032d512336eb4ffbabbd3133a2
TomDvirQM <tom.dvir@quantum-machines.co>
2025-08-22 11:11:59 +0200

    bug fix and updates to ramsey vs flux calibration

--- calibration_utils/ramsey_versus_flux_calibration/parameters.py
+++ calibration_utils/ramsey_versus_flux_calibration/parameters.py
@@ -6,15 +6,15 @@ from qualibration_libs.parameters import QubitsExperimentNodeParameters, CommonN
 class NodeSpecificParameters(RunnableParameters):
     num_shots: int = 100
     """Number of averages to perform. Default is 100."""
-    frequency_detuning_in_mhz: float = 1.0
+    frequency_detuning_in_mhz: float = 4.0
     """Frequency detuning in MHz. Default is 1.0 MHz."""
     min_wait_time_in_ns: int = 16
     """Minimum wait time in nanoseconds. Default is 16."""
-    max_wait_time_in_ns: int = 5000
+    max_wait_time_in_ns: int = 1000
     """Maximum wait time in nanoseconds. Default is 5000."""
-    wait_time_step_in_ns: int = 60
+    wait_time_step_in_ns: int = 8
     """Step size for the wait time scan in nanoseconds. Default is 60."""
-    flux_span: float = 0.01
+    flux_span: float = 0.05
     """Span of flux values to sweep in volts. Default is 0.01 V."""
     flux_num: int = 21
     """Number of flux points to sample. Default is 21."""
