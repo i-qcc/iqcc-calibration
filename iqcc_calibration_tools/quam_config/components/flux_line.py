b034373266d8f61c2baac49e5fa5c1433fa20d67
omrieoqm <omrie.ovdat@quantum-machines.co>
2025-08-28 15:42:42 +0300

    added some legacy nodes (because the new have some bugs) . Minor corrections

--- iqcc_calibration_tools/quam_config/components/flux_line.py
+++ iqcc_calibration_tools/quam_config/components/flux_line.py
@@ -2,7 +2,7 @@ from quam.core import quam_dataclass
 from typing import Dict, Any
 from dataclasses import field
 
-from quam_builder.architecture.superconducting.components.flux_line import FluxLine as QuamBuilderFluxLine
+from quam_builder.architecture.superconducting.components.flux_line import FluxLine as FluxLineBase
 
 from iqcc_calibration_tools.quam_config.lib.qua_utils import safe_wait
 
@@ -10,7 +10,7 @@ __all__ = ["FluxLine"]
 
 
 @quam_dataclass
-class FluxLine(QuamBuilderFluxLine):
+class FluxLine(FluxLineBase):
     """QuAM component for a flux line with custom configuration."""
 
     settle_time: float = 64 # ns 
