ae2827a3c71a7e4a4adccc4ddf519b96c0b1b348
TomDvirQM <tom.dvir@quantum-machines.co>
2025-09-30 09:08:50 +0200

    flux ampo control on XYZ delay

--- calibrations/14b_XY_Z_delay_4nS.py
+++ calibrations/14b_XY_Z_delay_4nS.py
@@ -8,7 +8,24 @@ from datetime import datetime, timezone, timedelta
 from qualang_tools.multi_user import qm_session
 from qualang_tools.results import fetching_tool, progress_counter
 from iqcc_calibration_tools.qualibrate_config.qualibrate.node import QualibrationNode, NodeParameters
-from typing import List, Optional, Literal
+from typing import Optional, Literal
+
+class Parameters(NodeParameters):
+    qubits: Optional[str] = None
+    num_averages: int = 100
+    delay_span: int = 70 # in clock cycles
+    flux_point_joint_or_independent: Literal['joint', 'independent'] = "joint"
+    reset_type_thermal_or_active: Literal['thermal', 'active'] = "thermal"
+    flux_amp: float = 0.1
+    simulate: bool = False
+    timeout: int = 100
+
+
+node = QualibrationNode(
+    name="14b_XY_Z_delay_4nS",
+    parameters=Parameters()
+)
+
 from qm.qua import *
 from qm import SimulationConfig
 from qualang_tools.units import unit
@@ -113,7 +130,7 @@ with program() as xy_z_delay_calibration:
                             qubit.xy.wait(qubit.xy.operations['x180'].length // 4)
                         qubit.z.wait(qubit.xy.operations['x180'].length // 4)
                         qubit.z.wait(node.parameters.delay_span)
-                        qubit.z.play("const")
+                        qubit.z.play("const", amplitude_scale=node.parameters.flux_amp/qubit.z.operations['const'].amplitude)
                         qubit.xy.wait(node.parameters.delay_span + t)
                         qubit.xy.play("x180")
 
